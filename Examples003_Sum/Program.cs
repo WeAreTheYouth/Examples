28be050f58b480d7acc29c2a8d3dbb665d9a7348
WeAreTheYouth <open8your5mind@gmail.com>
2022-09-03 06:26:36 +0300

    add Random

--- Examples003_Sum/Program.cs
+++ Examples003_Sum/Program.cs
@@ -1,3 +1,5 @@
-﻿int numberA = 4;
-int numberB = 8;
-Console.Write(numberA + numberB);
+﻿int numberA = new Random().Next(1, 25);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 25);
+Console.WriteLine(numberB);
+Console.WriteLine(numberA + numberB);
